f322892edfbfc73c0281aaa93043228aab43402e
Aluno <aluno@senai.cabb>
2026-01-27 17:27:46 -0300

    interaçao no site

---
+++ projeto2701/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Exercicio1 from "./componentes/Exercicio1";
+
+function App() {
+  return (
+    <div>
+      <Exercicio1 />
+    </div>
+  );
+}
+
+export default App;
